f89ed2bf0c577e2556413bc261f63378e900eb45
eybarr4 <eybarr14@stu.southtexascollege.edu>
2024-04-20 15:27:01 -0500

    Search by name is complete

--- src/store/actions.js
+++ src/store/actions.js
@@ -5,20 +5,20 @@ const { api } = useAPI()
 export function searchMeals({ commit }, keyword) {
     api.get(`search.php?s=${keyword}`)
         .then(({ data }) => {
-            commit('setSearchedMeals', data.meals)            
+        commit('setSearchedMeals', data.meals)            
     })
 }
 
 export function searchMealsByLetter({ commit }, letter) {
     api.get(`search.php?f=${letter}`)
         .then(({ data }) => {
-            commit('setMealsByLetter', data.meals)            
+        commit('setMealsByLetter', data.meals)            
     })
 }
 
-export function searchMealsByIngredients({ commit }, ingredient) {
+export function searchMealsByIngredient({ commit }, ingredient) {
     api.get(`filter.php?i=${ingredient}`)
         .then(({ data }) => { 
-            commit('setMealsByIngredients', data.meals)
+        commit('setMealsByIngredient', data.meals)
     })
 }
